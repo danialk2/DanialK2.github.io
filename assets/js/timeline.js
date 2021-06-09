925fcf6ae68de33c7da70499473a0200596ce4cc
danialk2 <55274358+danialk2@users.noreply.github.com>
2021-06-09 15:36:43 +0200

    Add files via upload
    initial commit

---
+++ assets/js/timeline.js
@@ -0,0 +1,106 @@
+ 
+     "use strict";
+
+function qs(selector, all = false) {
+  return all ? document.querySelectorAll(selector) : document.querySelector(selector);
+}
+
+const sections = qs('.section', true);
+const timeline = qs('.timeline');
+const line = qs('.line');
+line.style.bottom = `calc(100% - 20px)`;
+let prevScrollY = window.scrollY;
+let up, down;
+let full = false;
+let set = 0;
+const targetY = window.innerHeight * .8; 
+
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+$(document).on("click","a",function(e){
+  e.preventDefault();
+  var id = $(this).attr("href"),
+      topSpace = 30;
+//alert(id);
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - topSpace
+  }, 800);
+});
+
+overlay.addEventListener('click', ()=>{
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+
+function openModal(modal){
+  if(modal == null) return 
+  modal.classList.add('active')
+  overlay.classList.add('active')
+
+}
+
+function closeModal(modal){
+  if(modal == null) return 
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+
+}
+
+function scrollHandler(e) {
+  const {
+    scrollY
+  } = window;
+  up = scrollY < prevScrollY;
+  down = !up;
+  const timelineRect = timeline.getBoundingClientRect();
+  const lineRect = line.getBoundingClientRect(); // const lineHeight = lineRect.bottom - lineRect.top;
+
+  const dist = targetY - timelineRect.top;
+  console.log(dist);
+
+  if (down && !full) {
+    set = Math.max(set, dist);
+    line.style.bottom = `calc(100% - ${set}px)`;
+  }
+
+  if (dist > timeline.offsetHeight + 50 && !full) {
+    full = true;
+    line.style.bottom = `-50px`;
+  }
+
+  sections.forEach(item => {
+    // console.log(item);
+    const rect = item.getBoundingClientRect(); //     console.log(rect);
+
+    if (rect.top + item.offsetHeight / 5 < targetY) {
+      item.classList.add('show-me');
+    }
+  }); // console.log(up, down);
+
+  prevScrollY = window.scrollY;
+}
+
+scrollHandler();
+line.style.display = 'block';
+window.addEventListener('scroll', scrollHandler);
